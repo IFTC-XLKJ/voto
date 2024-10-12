a8702f1ed0c5255c4b64f5cb9d7b165d4e6eb3bb
IFTC-XLKJ <iftcceo@139.com>
2024-10-12 23:44:19 +0800

    Update toolbox.js

--- editor/toolbox.js
+++ editor/toolbox.js
@@ -1,4 +1,29 @@
 window.toolbox = {
     kind: "categoryToolbox",
-    contents: []
+    contents: [
+        {
+            kind: "category",
+            name: "事件",
+            contents: [
+                {
+                    kind: "block",
+                    type: "events_when_start",
+                },
+            ]
+        },
+        {
+            kind: "category",
+            name: "变量",
+            contents: [
+                {
+                    kind: "block",
+                    type: "variables_set",
+                },
+                {
+                    kind: "block",
+                    type: "variables_get",
+                },
+            ]
+        },
+    ]
 }
