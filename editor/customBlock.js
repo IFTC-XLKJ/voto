0bf1afb5aa953dec1eebc21160fe4e6f95b85c37
IFTC-XLKJ <iftcceo@139.com>
2024-10-12 21:16:59 +0800

    Create customBlock.js

---
+++ editor/customBlock.js
@@ -0,0 +1,9 @@
+class Block {
+    constructor(id, name, content, type, parent) {
+        this.id = id;
+        this.name = name;
+        this.content = content;
+        this.type = type;
+        this.parent = parent;
+    }
+}
