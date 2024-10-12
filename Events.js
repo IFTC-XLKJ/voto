2f133251ce8f6271067f3f6d1d9f308f3dde525a
IFTC-XLKJ <iftcceo@139.com>
2024-10-12 23:44:21 +0800

    Create Events.js

---
+++ Events.js
@@ -0,0 +1,19 @@
+class Events {
+    constructor() {
+        this.events = {};
+    }
+
+    on(event, callback) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+
+        this.events[event].push(callback);
+    }
+
+    emit(event, ...args) {
+        if (this.events[event]) {
+            this.events[event].forEach(callback => callback(...args));
+        }
+    }
+}
