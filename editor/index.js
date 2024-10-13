42c97e326d3a26676059d208e7f7547ac5595168
IFTC-XLKJ <iftcceo@139.com>
2024-10-13 11:12:16 +0800

    Update index.js

--- editor/index.js
+++ editor/index.js
@@ -39,15 +39,26 @@ addEventListener("load", () => {
         });
         let blocksBoxes = []
         var blockly0 = document.getElementById("blockly-0")
-        blocksBoxes.push(blockly0)
+        blocksBoxes.push({ blockly: blockly0, color: "#608FEEFF" })
+        var blockly1 = document.getElementById("blockly-1")
+        blocksBoxes.push({ blockly: blockly1, color: "#68CDFFFF" })
+        var blockly2 = document.getElementById("blockly-2")
+        blocksBoxes.push({ blockly: blockly2, color: "#FFA500FF" })
         blocksBoxes.forEach(blockly => {
-            blockly.style.borderLeft = "8px solid #608FEEFF"
-            blockly.style.backgroundColor = "#FFFFFF00"
-            blockly.addEventListener("click", () => {
-                if (blockly.style.backgroundColor == "#608FEEFF") {
-                    blockly.style.backgroundColor = "#FFFFFF00"
+            blockly.blockly.style.borderLeft = "8px solid " + blockly.color
+            blockly.blockly.style.backgroundColor = "#FFFFFF00"
+            blockly.blockly.addEventListener("click", () => {
+                const blocklyTreeLabels = document.querySelectorAll(".blocklyTreeLabel")
+                blocklyTreeLabels.forEach(blocklyTreeLabel => {
+                    blocklyTreeLabel.style.color = "#333"
+                })
+                const blocklyTreeLabel = blockly.blockly.querySelector(".blocklyTreeLabel")
+                if (blockly.blockly.style.backgroundColor == blockly.color) {
+                    blockly.blockly.style.backgroundColor = "#FFFFFF00"
+                    blocklyTreeLabel.style.color = "#333"
                 } else {
-                    blockly.style.backgroundColor = "#608FEEFF"
+                    blockly.blockly.style.backgroundColor = blockly.color
+                    blocklyTreeLabel.style.color = "white"
                 }
             })
         })
@@ -70,9 +81,14 @@ addEventListener("load", () => {
         } catch (err) {
             console.log(err)
         }
-        //Blockly.Variables.createVariableButtonHandler(button.getTargetWorkspace(), null, 'panda');
+        workspace.registerButtonCallback("createVar", function (ws) {
+            Blockly.Variables.createVariableButtonHandler(workspace, null, 'any')
+        })
         workspace.addChangeListener(function (event) {
             console.log(event)
+            if (event.type == "var_create") {
+                console.log(`Variable "${event.varName}" ID: "${event.varId}"`);
+            }
             if (event.type == "finished_loading") {
                 console.log("加载")
                 isLoaded = true
@@ -82,45 +98,67 @@ addEventListener("load", () => {
                     console.log("保存")
                     localStorage.setItem("blocklyData", JSON.stringify(Blockly.serialization.workspaces.save(workspace)))
                 }
+                const blocklyTreeLabels = document.querySelectorAll(".blocklyTreeLabel")
+                blocklyTreeLabels.forEach(blocklyTreeLabel => {
+                    blocklyTreeLabel.style.color = "#333"
+                })
             }
-            const blocklyBlockCanvas = document.querySelector(".blocklyBlockCanvas")
-            const CanvasX = blocklyBlockCanvas.transform.animVal[0].matrix.e;
-            const CanvasY = blocklyBlockCanvas.transform.animVal[0].matrix.f;
-            localStorage.setItem("blocklyCanvas", JSON.stringify({ x: CanvasX, y: CanvasY }))
-            console.log(CanvasX, CanvasY)
-            const code = javascriptGenerator.workspaceToCode(workspace);
-            console.log(code)
-        })
-        setInterval(() => {
-            const blocklyDropDownDiv = document.querySelector(".blocklyDropDownDiv")
-            if (blocklyDropDownDiv) {
-                blocklyDropDownDiv.style.backgroundColor = "white"
-                blocklyDropDownDiv.style.borderColor = "white"
+            if (event.type == "click") {
+                const blocklyTreeLabels = document.querySelectorAll(".blocklyTreeLabel")
+                blocklyTreeLabels.forEach(blocklyTreeLabel => {
+                    blocklyTreeLabel.style.color = "#333"
+                })
             }
-            const blocklyMenuItemContents = document.querySelectorAll(".blocklyMenuItemContent")
-            blocklyMenuItemContents.forEach(blocklyMenuItemContent => {
-                blocklyMenuItemContent.style.color = "#333"
-            })
-            const blocklyFlyoutButtonShadow = document.querySelector(".blocklyFlyoutButtonShadow")
-            if (blocklyFlyoutButtonShadow) {
-                blocklyFlyoutButtonShadow.setAttribute("width", 200)
-                blocklyFlyoutButtonShadow.setAttribute("height", 30)
-            }
-            const blocklyFlyoutButtonBackground = document.querySelector(".blocklyFlyoutButtonBackground")
-            if (blocklyFlyoutButtonBackground) {
-                blocklyFlyoutButtonBackground.setAttribute("width", 200)
-                blocklyFlyoutButtonBackground.setAttribute("height", 30)
-            }
-            const blocklyText = document.querySelector(".blocklyFlyoutButton>.blocklyText")
-            if (blocklyText) {
-                blocklyText.setAttribute("y", 21)
-                blocklyText.setAttribute("x", 100)
-            }
-            const blocklyPath2 = document.querySelectorAll(".blocklySelected>.blocklyPath")[1]
-            if (blocklyPath2) {
-                blocklyPath2.setAttribute("d", "")
-                blocklyPath2.setAttribute("fill", "none")
-            }
-        }, 1)
+        const blocklyBlockCanvas = document.querySelector(".blocklyBlockCanvas")
+        const CanvasX = blocklyBlockCanvas.transform.animVal[0].matrix.e;
+        const CanvasY = blocklyBlockCanvas.transform.animVal[0].matrix.f;
+        localStorage.setItem("blocklyCanvas", JSON.stringify({ x: CanvasX, y: CanvasY }))
+        console.log(CanvasX, CanvasY)
+        const code = javascriptGenerator.workspaceToCode(workspace);
+        console.log(code)
     })
+    setInterval(() => {
+        const blocklyDropDownDiv = document.querySelector(".blocklyDropDownDiv")
+        if (blocklyDropDownDiv) {
+            blocklyDropDownDiv.style.backgroundColor = "white"
+            blocklyDropDownDiv.style.borderColor = "white"
+        }
+        const blocklyMenuItemContents = document.querySelectorAll(".blocklyMenuItemContent")
+        blocklyMenuItemContents.forEach(blocklyMenuItemContent => {
+            blocklyMenuItemContent.style.color = "#333"
+        })
+        const blocklyFlyoutButtonShadow = document.querySelector(".blocklyFlyoutButtonShadow")
+        if (blocklyFlyoutButtonShadow) {
+            blocklyFlyoutButtonShadow.setAttribute("width", 200)
+            blocklyFlyoutButtonShadow.setAttribute("height", 30)
+        }
+        const blocklyFlyoutBackground = document.querySelector(".blocklyFlyoutBackground")
+        if (!blocklyFlyoutBackground) {
+            const blocklyTreeLabels = document.querySelectorAll(".blocklyTreeLabel")
+            blocklyTreeLabels.forEach(blocklyTreeLabel => {
+                blocklyTreeLabel.style.color = "#333"
+            })
+        }
+        const blocklyFlyoutButtonBackground = document.querySelector(".blocklyFlyoutButtonBackground")
+        if (blocklyFlyoutButtonBackground) {
+            blocklyFlyoutButtonBackground.setAttribute("width", 200)
+            blocklyFlyoutButtonBackground.setAttribute("height", 30)
+        }
+        const blocklyText = document.querySelector(".blocklyFlyoutButton>.blocklyText")
+        if (blocklyText) {
+            blocklyText.setAttribute("y", 21)
+            blocklyText.setAttribute("x", 100)
+        }
+        const blocklyPath2 = document.querySelectorAll(".blocklySelected>.blocklyPath")[1]
+        if (blocklyPath2) {
+            blocklyPath2.setAttribute("d", "")
+            blocklyPath2.setAttribute("fill", "none")
+        }
+        const blocklyPath4 = document.querySelectorAll(".blocklySelected>.blocklyPath")[3]
+        if (blocklyPath4) {
+            blocklyPath4.setAttribute("d", "")
+            blocklyPath4.setAttribute("fill", "none")
+        }
+    }, 1)
+})
 })
