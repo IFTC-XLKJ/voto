624741a649cf93c767e17c1d0eaa0a0b212b4944
IFTC-XLKJ <iftcceo@139.com>
2024-10-18 23:31:10 +0800

    Update blocks.js

--- editor/blocks.js
+++ editor/blocks.js
@@ -35,7 +35,11 @@ addEventListener('load', function () {
             .appendField('');
         this.setOutput(false, "String");
         this.svgGroup_.classList.add('EventsBlocks');
-    }, [null])
+    }, {
+        role: function (block) {
+            return roles
+        }
+    })
     block.code("events_role_event", function (block) {
         var role = block.getFieldValue("role")
         var eventName = block.getFieldValue("eventName")
@@ -103,6 +107,76 @@ addEventListener('load', function () {
         init: function () {
             this.jsonInit(ControlsIfJson);
             this.svgGroup_.classList.add('ControlsBlocks');
+        }
+    };
+    block.add("controls_output", function () {
+        console.log(this)
+        this.appendDummyInput()
+            .appendField('在控制台')
+            .appendField(new Blockly.FieldDropdown([
+                ["打印", "print"],
+                ["警告", "warn"],
+                ["错误", "error"]
+            ]), "type")
+            .appendField(new Blockly.FieldTextInput("调试信息"), "text");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.svgGroup_.classList.add('ControlsBlocks');
+    }, [null])
+    block.code("controls_output", function (block) {
+        var type = block.getFieldValue("type")
+        var text = block.getFieldValue("text")
+        var code = `Csl.${type}(${text})`
+        return code
+    })
+    // Variables
+    var VariablesGetJson = {
+        type: 'variables_get',
+        message0: '%1',
+        args0: [
+            {
+                type: 'field_variable',
+                name: 'VAR',
+                variable: '%{BKY_VARIABLES_DEFAULT_NAME}',
+            },
+        ],
+        output: null,
+        style: 'variable_blocks',
+        helpUrl: '%{BKY_VARIABLES_GET_HELPURL}',
+        tooltip: '%{BKY_VARIABLES_GET_TOOLTIP}',
+        extensions: ['contextMenu_variableSetterGetter'],
+    }
+    Blockly.Blocks['variables_get'] = {
+        init: function () {
+            this.jsonInit(VariablesGetJson);
+            this.svgGroup_.classList.add('VariablesBlocks');
+        }
+    };
+    var VariablesSetJson = {
+        type: 'variables_set',
+        message0: '%{BKY_VARIABLES_SET}',
+        args0: [
+            {
+                type: 'field_variable',
+                name: 'VAR',
+                variable: '%{BKY_VARIABLES_DEFAULT_NAME}',
+            },
+            {
+                type: 'input_value',
+                name: 'VALUE',
+            },
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        style: 'variable_blocks',
+        tooltip: '%{BKY_VARIABLES_SET_TOOLTIP}',
+        helpUrl: '%{BKY_VARIABLES_SET_HELPURL}',
+        extensions: ['contextMenu_variableSetterGetter'],
+    }
+    Blockly.Blocks['variables_set'] = {
+        init: function () {
+            this.jsonInit(VariablesSetJson);
+            this.svgGroup_.classList.add('VariablesBlocks');
         }
     };
     console.log("blockLoad")
