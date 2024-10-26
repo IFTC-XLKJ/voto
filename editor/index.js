f8d52f9287b34a20c093dc3e9fb24ac9f401fbc4
IFTC-XLKJ <iftcceo@139.com>
2024-10-26 20:40:15 +0800

    Update index.js

--- editor/index.js
+++ editor/index.js
@@ -16,8 +16,8 @@ window.workdata = {
         {
             name: "role",
             url: "/assets/role.svg",
-            x: 0,
-            y: 0,
+            x: 100,
+            y: 50,
             width: 100,
             height: 100
         }
