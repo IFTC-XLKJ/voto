e73e9b88e00f2dd3278b46d297bab66d27c5fc9d
IFTC-XLKJ <iftcceo@139.com>
2024-11-10 15:40:52 +0800

    Update index.js

--- home/index.js
+++ home/index.js
@@ -17,7 +17,7 @@ addEventListener("load", () => {
         loginBtn.innerText = "你还未登录，点击登录"
         loginBtn.style.cssText = "width:280px;height:40px;margin:20px auto;background-color: #007BFF;color: #fff;border: none;border-radius: 4px;cursor: pointer;font-size: 16px;display: flex;justify-content: center;align-items: center;"
         loginBtn.onclick = () => {
-            location.href = "login.html#home"
+            location.href = "/login.html#home"
         }
         works.appendChild(loginBtn)
     }
