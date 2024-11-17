2f43425b9d8ccc779e3c4df2d5a0059d7b20485c
IFTC-XLKJ <iftcceo@139.com>
2024-11-17 13:21:52 +0800

    Update Toast.class.js

--- Toast.class.js
+++ Toast.class.js
@@ -1,20 +1,23 @@
-function mathRandomInt(a, b) {
+const mathRandomInt = (a, b) => {
     if (a > b) {
         var c = a;
         a = b;
-        b = c; parent
+        b = c;
+        parent
     }
     return Math.floor(Math.random() * (b - a + 1) + a);
 }
 
 class Toast {
     constructor() {
-        if (!document.querySelector('link[href="/Toast.css"]')) {
+        if (!document.querySelector('link[href="./Toast.css"]')) {
             const link = document.createElement("link");
             link.rel = "stylesheet";
-            link.href = "/Toast.css";
+            link.href = "./Toast.css";
             document.head.appendChild(link);
         }
+
+        this.toasts = [];
     }
 
     normal(text, time) {
@@ -25,12 +28,15 @@ class Toast {
             const toast = document.getElementById(id);
             if (toast) {
                 toast.remove();
+                this.toasts = this.toasts.filter(toast => toast != id);
             }
         }, time);
+        this.toasts.push(id);
+        return id;
     }
 
     success(text, time) {
-        const id = `toast${mathRandomInt(100000, 999999)}`
+        const id = `toast${mathRandomInt(100000, 999999)}`;
         const html = `<div class="success-toast" id="${id}">${text}</div>`;
         document.body.insertAdjacentHTML('beforeend', html);
         var toast = document.getElementById(id);
@@ -38,12 +44,15 @@ class Toast {
             const toast = document.getElementById(id);
             if (toast) {
                 toast.remove();
+                this.toasts = this.toasts.filter(toast => toast != id);
             }
         }, time);
+        this.toasts.push(id);
+        return id;
     }
 
     error(text, time) {
-        const id = `toast${mathRandomInt(100000, 999999)}`
+        const id = `toast${mathRandomInt(100000, 999999)}`;
         const html = `<div class="error-toast" id="${id}">${text}</div>`;
         document.body.insertAdjacentHTML('beforeend', html);
         var toast = document.getElementById(id);
@@ -51,13 +60,16 @@ class Toast {
             const toast = document.getElementById(id);
             if (toast) {
                 toast.remove();
+                this.toasts = this.toasts.filter(toast => toast != id);
             }
         }, time);
+        this.toasts.push(id);
+        return id;
     }
 
 
     warn(text, time) {
-        const id = `toast${mathRandomInt(100000, 999999)}`
+        const id = `toast${mathRandomInt(100000, 999999)}`;
         const html = `<div class="warn-toast" id="${id}">${text}</div>`;
         document.body.insertAdjacentHTML('beforeend', html);
         var toast = document.getElementById(id);
@@ -65,7 +77,72 @@ class Toast {
             const toast = document.getElementById(id);
             if (toast) {
                 toast.remove();
+                this.toasts = this.toasts.filter(toast => toast != id);
             }
         }, time);
+        this.toasts.push(id);
+        return id;
+    }
+
+    loading(text) {
+        const id = `toast${mathRandomInt(100000, 999999)}`;
+        const html = `<div class="toast-mask"><div class="loading-toast" id="${id}">
+<div class="dot-spinner">
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+</div><div class="toast-loading-text">${text}</div></div></div>`;
+        document.body.insertAdjacentHTML('beforeend', html);
+        var toast = document.getElementById(id);
+        this.toasts.push(id);
+        return id;
+    }
+
+    loadend(id) {
+        const toast = document.getElementById(id);
+        const mask = toast.parentElement;
+        if (toast) {
+            mask.remove();
+            this.toasts = this.toasts.filter(toast => toast != id);
+        }
+    }
+
+    cancel(id) {
+        if (!id) {
+            this.toasts.forEach(id => {
+                const toast = document.getElementById(id);
+                if (toast) {
+                    toast.remove();
+                    this.toasts = this.toasts.filter(toast => toast != id);
+                }
+            })
+            return 0;
+        }
+        const toast = document.getElementById(id);
+        if (toast) {
+            toast.remove();
+            this.toasts = this.toasts.filter(toast => toast != id);
+        }
+    }
+    
+    custom(color, bgcolor, text, time) {
+        const id = `toast${mathRandomInt(100000, 999999)}`;
+        const html = `<div class="custom-toast" style="--color: ${color};--bgcolor: ${bgcolor};" id="${id}">${text}</div>`;
+        document.body.insertAdjacentHTML('beforeend', html);
+        var toast = document.getElementById(id);
+        setTimeout(() => {
+            const toast = document.getElementById(id);
+            if (toast) {
+                toast.remove();
+                this.toasts = this.toasts.filter(toast => toast != id);
+            }
+        }, time);
+        this.toasts.push(id);
+        return id;
     }
 }
