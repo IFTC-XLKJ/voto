eb3a29e0004ae78e7b4f368c2a65c1f4e08d6f56
IFTC-XLKJ <iftcceo@139.com>
2024-11-17 12:59:13 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,18 @@
 window.isLogin = false
 onload = () => {
+    const toast = new Toast();
     const Submit = document.querySelector(".submit>button");
     var vvzh = new pgdbs(dbs_a6b2a4d6c02022e831626d31ab805a468a151b90d5161660485a73cc6e1ea902);
-    setInterval(() => {
+    setInterval(async () => {
         if (localStorage.getItem("token") && localStorage.getItem("UID") && localStorage.getItem("PWD") && isLogin != 10) {
             isLogin = true
+            toast.warn("你已登录过了，无法重复登录", 500)
+            await new Promise(resolve => setTimeout(resolve, 500))
             if (location.hash) {
                 window.location.href = location.hash.slice(1);
             } else {
                 window.location.href = "home";
             }
-            alert("你已登录过了，无法重复登录")
         }
     }, 200)
     Submit.onclick = (e) => {
@@ -19,25 +21,26 @@ onload = () => {
             limit: 1,
             page: 1,
             filter: `ID="${username.value}" OR 昵称="${username.value}" OR 邮箱="${username.value}" AND 密码="${CryptoJS.MD5(password.value)}"`
-        }).then(data => {
+        }).then(async data => {
             if (data.code == 200) {
                 if (data.fields.length == 0) {
-                    alert("ID/邮箱/ID或密码错误或者该账号不存在");
+                    toast.error("ID/邮箱/ID或密码错误或者该账号不存在", 2000);
                 } else {
                     localStorage.setItem("token", data.fields[0].token);
                     localStorage.setItem("UNM", data.fields[0].昵称);
                     localStorage.setItem("UID", data.fields[0].ID);
                     localStorage.setItem("PWD", password.value);
+                    toast.success("登录成功", 2000);
+                    await new Promise(resolve => setTimeout(resolve, 2000))
                     if (location.hash) {
                         window.location.href = location.hash.slice(1);
                     } else {
                         window.location.href = "home";
                     }
                     isLogin = 10;
-                    alert("登录成功");
                 }
             } else {
-                alert("登录失败，原因" + data.msg);
+                toast.error("登录失败，原因" + data.msg);
                 console.error(data);
             }
         })
