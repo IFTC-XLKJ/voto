ec745263a982169bb32199b848d4637db00a4131
IFTC-XLKJ <iftcceo@139.com>
2024-11-17 14:18:19 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// 定义一个简单的路由
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// 创建一个API端点
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from API!' });
+});
+
+// 启动服务器
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
