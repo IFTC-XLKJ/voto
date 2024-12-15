01f36923d2f4731324b0e76ee792e73c7d852704
IFTC-XLKJ <iftcceo@139.com>
2024-12-15 14:30:19 +0800

    Update blocks.js

--- editor/blocks.js
+++ editor/blocks.js
@@ -280,6 +280,64 @@ addEventListener('load', function () {
     })
     // Sound
     // Operators
+    var CompareJson = {
+        type: 'logic_compare',
+        message0: '%1 %2 %3',
+        args0: [
+            {
+                type: 'input_value',
+                name: 'A',
+            },
+            {
+                type: 'field_dropdown',
+                name: 'OP',
+                options: [
+                    ['=', 'EQ'],
+                    ['\u2260', 'NEQ'],
+                    ['\u200F<', 'LT'],
+                    ['\u200F\u2264', 'LTE'],
+                    ['\u200F>', 'GT'],
+                    ['\u200F\u2265', 'GTE'],
+                ],
+            },
+            {
+                type: 'input_value',
+                name: 'B',
+            },
+        ],
+        inputsInline: true,
+        output: 'Boolean',
+        tooltip: '比较两个值',
+    }
+    Blockly.Blocks['logic_compare'] = {
+        init: function () {
+            this.jsonInit(CompareJson);
+            this.svgGroup_.classList.add('OperatorsBlocks');
+        }
+    };
+    var BooleanJson = {
+        type: 'logic_boolean',
+        message0: '%1',
+        args0: [
+            {
+                type: 'field_dropdown',
+                name: 'BOOL',
+                options: [
+                    ['成立', 'TRUE'],
+                    ['不成立', 'FALSE'],
+                ],
+            },
+        ],
+        output: 'Boolean',
+        style: 'logic_blocks',
+        tooltip: '布尔值',
+    }
+    Blockly.Blocks['logic_boolean'] = {
+        init: function () {
+            this.jsonInit(BooleanJson);
+            this.svgGroup_.classList.add('OperatorsBlocks');
+        }
+    };
     var OperatorsTextJson = {
         type: 'text',
         message0: '“%1”',
