e6ae9d2b2660730c599958f1a23b147e563a81b9
IFTC-XLKJ <iftcceo@139.com>
2024-12-20 20:21:17 +0800

    Update Action.js

--- Action.js
+++ Action.js
@@ -11,15 +11,16 @@ class Action {
         console.log(this)
     }
     move(role, direction, distance) {
+        const preRun = document.querySelector("[data-type=\"run\"]")
         if (role != "__background__") {
             if (direction == "forward") {
                 this.roles[role].style.left = `${this.#getPer(this.roles[role]) + distance}%`
             } else if (direction == "backward") {
-                this.roles[role].style.left = `${(this.roles[role].offsetLeft + distance) / (preview.clientWidth / 640)}px`
+                this.roles[role].style.left = `${(this.roles[role].offsetLeft + distance) / (preRun.clientWidth / 640)}px`
             } else if (direction == "upward") {
-                this.roles[role].style.top = `${(this.roles[role].offsetTop - distance) / (preview.clientHeight / 360)}px`
+                this.roles[role].style.top = `${(this.roles[role].offsetTop - distance) / (preRun.clientHeight / 360)}px`
             } else if (direction == "downward") {
-                this.roles[role].style.top = `${(this.roles[role].offsetTop + distance) / (preview.clientHeight / 360)}px`
+                this.roles[role].style.top = `${(this.roles[role].offsetTop + distance) / (preRun.clientHeight / 360)}px`
             }
         } else {
             parentWindow.Csl.error("背景不是角色\n背景不能移动")
